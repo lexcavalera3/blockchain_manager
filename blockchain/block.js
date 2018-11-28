7c692b5c35b15ae0a7331b44b24077c2b0656b31
lexcavalera <lexcavalera3@gmail.com>
2018-11-28 09:18:23 +0200

    Add eslint configuration to the project
    And update code to meet google style requirements

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,49 +1,67 @@
-const sha256 = require("js-sha256").sha256;
+const sha256 = require('js-sha256').sha256;
 
-
+/**
+ * Block - the core of blockchain.
+ * Stores data along with index, creation timestamp and information
+ * about previous block.
+ */
 class Block {
+  /**
+   * @param {number} index - The index of the block.
+   * @param {string} data - Information, that should be stored.
+   * @param {string} previousHash - Previous blocks' hash.
+   */
+  constructor(index, data, previousHash) {
+    this.index = index;
+    this.timestamp = new Date();
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this._hashBlock();
+  }
 
-    constructor(index, timestamp, data, previousHash) {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.hashBlock();
-    }
-
-    hashBlock() {
-        return sha256(
-            this.index.toString()
-            + this.timestamp.toString()
-            + this.data.toString()
-            + this.previousHash.toString());
-    }
+  /**
+   * Generates unique hash for this block, that depends on all its' fields.
+   * @return {string} New hash.
+   */
+  _hashBlock() {
+    return sha256(
+        this.index.toString()
+        + this.timestamp.toString()
+        + this.data.toString()
+        + this.previousHash.toString());
+  }
 }
 
-
+/**
+ * Generates first block of the blockchain.
+ * @return {object} First block.
+ */
 function createGenesisBlock() {
-    return new Block(0, new Date(), "Genesis Block", "0");
+  return new Block(0, 'Genesis Block', '0');
 }
 
-
+/**
+ * Generates next block of the blockchain
+ * @param {object} lastBlock - The last block of blockchain.
+ * @return {object} Next block.
+ */
 function nextBlock(lastBlock) {
-    let index = lastBlock.index + 1;
-    let timestamp = new Date();
-    let data = "Hey! I'm block " + index.toString();
-    let lastHash = lastBlock.hash;
-    return new Block(index, timestamp, data, lastHash);
+  const index = lastBlock.index + 1;
+  const data = 'Hey! I\'m block ' + index.toString();
+  const lastHash = lastBlock.hash;
+  return new Block(index, data, lastHash);
 }
 
 
 let previousBlock = createGenesisBlock();
 let blockToAdd = null;
-let blockchain = [previousBlock];
+const blockchain = [previousBlock];
 const blocksToAdd = 10;
 
-for (var i = 1; i < blocksToAdd; i++) {
-    blockToAdd = nextBlock(previousBlock);
-    blockchain.push(blockToAdd);
-    previousBlock = blockToAdd;
-    console.log(`Block ${blockToAdd.index} has been added to the blockchain!`);
-    console.log(`Hash: ${blockToAdd.hash}\n`);
+for (let i = 1; i < blocksToAdd; i++) {
+  blockToAdd = nextBlock(previousBlock);
+  blockchain.push(blockToAdd);
+  previousBlock = blockToAdd;
+  console.log(`Block ${blockToAdd.index} has been added to the blockchain!`);
+  console.log(`Hash: ${blockToAdd.hash}\n`);
 }
